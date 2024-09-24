05aae89cb91a536acac39bb32f906ac6136ee3b9
Shermalyn Andon <izhameandon@gmail.com>
2024-09-24 16:49:04 -0400

    added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author: Shermalyn Andon
+# Author ID: 160564217
+# Date Created: 2024/09/24
+
+import sys
+
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer -= 1 
+
+print('blast off!')
