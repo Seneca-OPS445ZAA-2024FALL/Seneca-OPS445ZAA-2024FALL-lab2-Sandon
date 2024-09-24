563db6ebd5adf0724c59696a1dcd5d4af3d9a8b5
Shermalyn Andon <izhameandon@gmail.com>
2024-09-24 16:46:00 -0400

    added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Shermalyn Andon
+# Author ID: 160564217
+# Date Created: 2024/09/24
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print('blast off!')
